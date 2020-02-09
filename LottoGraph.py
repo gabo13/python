7f59c3ab4a608677cda6fe8b9168b04b41dcb2f4
gabo13 <horvathg83@gmail.com>
2020-02-09 15:14:16 +0100

    mouse

---
+++ LottoGraph.py
@@ -0,0 +1,53 @@
+﻿# -*- Coding:Utf-8 -*-
+# Lotto graphics application
+# Created by Horváth Gábor
+# Hungary
+# 2020. 02. 01.
+
+import urllib
+import Tkinter as tk
+
+class MainFrame(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        
+        T = tk.Text(self, height=2, width=10)
+        
+        T.pack()
+        T.insert(tk.END, 'This is a text')
+        
+
+class LottoGraph():
+
+    
+    
+    def __init__(self):
+        URL = 'https://bet.szerencsejatek.hu/cmsfiles/otos.csv'
+        self.data = []
+        
+        with open('otos.csv','wb') as f:
+            f.write(urllib.urlopen(URL).read())
+            f.close()
+
+        
+
+        with open('otos.csv','r') as f:
+            for line in f:
+                temp = list(line.rstrip().split(';'))
+                temp2 = map(int,temp[0:2]+temp[11:15])
+                self.data.append(temp2)
+            f.close()
+
+        #self.show()
+
+    def show(self):
+        print(self.data)
+        #for lista in data:
+            #print(' '.join(map(str,lista)))
+
+
+if __name__ == '__main__':
+    LottoGraph()
+    root = tk.Tk()
+    MainFrame().pack(root)
+    root.mainloop()
---
+++ mouse.py
@@ -0,0 +1,53 @@
+# -*- coding: Utf-8 -*-
+
+from Tkinter import *
+from random import randrange
+
+class Draw(Frame):
+    "A program főablakát definiáló osztály"
+    def __init__(self):
+        Frame.__init__(self)
+        # A vászon létrehozása
+        self.c = Canvas(self, width=400, height=300, bg='ivory')
+        self.c.pack(padx=5, pady=3)
+        for i in range(15):
+            # Véletlenszerüen kisorsolunk egy színt
+            coul=['brown','red','orange','yellow','green','cyan','blue','violet','purple',][randrange(9)]
+            # Véletlen koordinátájú elipszis rajzolása
+            x1, y1 = randrange(300), randrange(200)
+            x2, y2 = x1 + randrange(10,150), y1 +randrange(10,150)
+            self.c.create_oval(x1, y1, x2, y2, fill=coul)
+
+        # Az <egéresemények hozzákapcsolása a <canvas> widgethez
+        self.c.bind("<Button-1>", self.mouseDown)
+        self.c.bind("<Button1-Motion>", self.mouseMove)
+        self.c.bind("<Button1-ButtonRelease>", self.mouseUp)
+        # A kilépésgomb létrehozása
+        b_fin = Button(self, text='Befejezés', bg='royal blue', fg='white',  font=('Helvetica',10,'bold'), command=self.quit)
+        b_fin.pack(pady=2)
+        self.pack()
+
+    def mouseDown(self, event):
+        "Bal egérgomb lenyomására végrehajtandó művelet"
+        self.currObject = None
+        self.x1, self.y1 = event.x, event.y
+        self.selObject = self.c.find_closest(self.x1, self.y1)
+        self.c.itemconfig(self.selObject, width=3)
+        self.c.lift(self.selObject)
+
+    def mouseMove(self, event):
+        "Lenyomott bal gomb mozgó egérrel végrehajtandó művelet"
+        x2, y2 = event.x, event.y
+        dx, dy = x2-self.x1, y2-self.y1
+        if self.selObject:
+            self.c.move(self.selObject, dx, dy)
+            self.x1, self.y1 = x2, y2
+
+    def mouseUp(self, event):
+        "A bal egérgomb fölengedésekor végrehajtandó művelet"
+        if self.selObject:
+            self.c.itemconfig(self.selObject, width=1)
+            self.selObject = None
+
+if __name__ == '__main__':
+    Draw().mainloop()
